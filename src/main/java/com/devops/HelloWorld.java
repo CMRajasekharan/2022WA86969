78b5ca55bd0324b676dd6754437aa30198c284c8
C M Rajasekharan <2022wa86969@wilp.bits-pilani.ac.in>
2026-09-20 14:07:09 +0530

    Modified HelloWorld message for CI demonstration

--- src/main/java/com/devops/HelloWorld.java
+++ src/main/java/com/devops/HelloWorld.java
@@ -1,8 +1,13 @@
 package com.devops;
 
+package com.devops;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Lab!");
+        System.out.println("Continuous Integration using Jenkins");
+        System.out.println("Source code successfully modified!");
     }
 }
+
